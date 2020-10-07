b91e30303b7d3e6d762fccc0bee47ce101b34b56
brahim mahious <brahim.mahious@hetic.net>
2020-10-07 22:47:33 +0200

    starting project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomePage from './homePage.component';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <HomePage />  
+    </div>
+  );
+}
+
+export default App;
